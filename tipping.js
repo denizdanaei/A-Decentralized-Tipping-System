f8218baafa94231ea5576c8e1f3b2c515e2b5e3c
Lennart Overdevest <lennart_basketbal@msn.com>
2020-03-03 00:34:28 +0100

    Very basic working plugin which adds a div containing a button to the current html page and alerts a message when the button is clicked.

---
+++ tipping.js
@@ -0,0 +1,17 @@
+function donateMoney() {
+  alert("Thank you for your donation!")
+}
+
+document.getElementById("container").innerHTML = `<div class="popup" style="position: absolute;  top: 80px;
+right: 80px; width:20%;">
+<div style="position:absolute; top: 50%;left: 50%; border-style: solid; background-color: lightblue;">
+  <span class="popuptext" id="myPopup" style"text-align:center;"><b>Do you want to tip?</b></span>
+  <br>
+  <button type = "button" id = "donateButton"> Tip here </button>  
+  </span>
+  </div>
+</div>`;
+document.getElementById("donateButton").addEventListener("click", donateMoney);
+
+
+
