e6d7137b8b95082ef6cd654a16b25a2086189341
Lennart Overdevest <lennart_basketbal@msn.com>
2020-03-10 19:49:45 +0100

    First connection XRP Ledger via button click

--- tipping.js
+++ tipping.js
@@ -2,6 +2,30 @@
 function donateMoney() {  
   // var address = document.getElementById("address").innerHTML;
   alert("Thank you for your donation!" + "\n \nThe tip will be send to: " + getPublicAddress())
+  printXrpConnection()
+}
+
+function printXrpConnection() {
+  var api = new ripple.RippleAPI({server:'wss://s1.ripple.com/'});
+  api.connect().then(function() {
+      return api.getServerInfo();
+  }).then(function(server_info) {
+  document.body.innerHTML += "<p>Connected to rippled server!</p>" +
+"      <table>" +
+"        <tr><th>Version</th>" +
+"          <td>" + server_info.buildVersion + "</td></tr>" +
+"        <tr><th>Ledgers available</th>" +
+"          <td>" + server_info.completeLedgers + "</td></tr>" +
+"        <tr><th>hostID</th>" +
+"          <td>" + server_info.hostID + "</td></tr>" +
+"        <tr><th>Most Recent Validated Ledger Seq.</th>" +
+"          <td>" + server_info.validatedLedger.ledgerVersion + "</td></tr>" +
+"        <tr><th>Most Recent Validated Ledger Hash</th>" +
+"          <td>" + server_info.validatedLedger.hash + "</td></tr>" +
+"        <tr><th>Seconds since last ledger validated</th>" +
+"          <td>" + server_info.validatedLedger.age + "</td></tr>" +
+"      </table>";
+  });
 }
 
 // Funtion to get a specifc meta value
