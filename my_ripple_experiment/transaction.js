05505fdc4932928536ec6f567654f28a9c743cc8
Lennart Overdevest <lennart_basketbal@msn.com>
2020-03-11 22:41:03 +0100

    Working version including file uploading and deleted wrong index.html

--- my_ripple_experiment/transaction.js
+++ my_ripple_experiment/transaction.js
@@ -1,6 +1,14 @@
 
-function doTransaction(publicAddress) {
-    console.log(publicAddress)
+function doTransaction(receiverAddress, senderAddress, privateKey) {
+    console.log("Public address webpage " + receiverAddress)
+    console.log("Public address user " + senderAddress)
+    console.log("Private key user " + privateKey)
+
+    // For testing overwrite the arguments
+    receiverAddress = 'rUCzEr6jrEyMpjhs4wSdQdz4g8Y382NxfM'
+    senderAddress = 'rB76Ts8bXWnZVkqbFULYpwV9CaUaCFZ6hn'
+    privateKey = 'ss7ktBumXYJAF9PhkHmwhEUCD7Ldc'
+
     api = new ripple.RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'})
     var maxLedgerVersion = null
     var txID = null
@@ -14,7 +22,7 @@ function doTransaction(publicAddress) {
         doPrepare().then(txJSON => {  
         
         //The third step is to sign the JSON file with the secret key
-        const response = api.sign(txJSON, "ss7ktBumXYJAF9PhkHmwhEUCD7Ldc")
+        const response = api.sign(txJSON, privateKey)
         txID = response.id
         console.log("Identifying hash:", txID)
         const txBlob = response.signedTransaction
@@ -37,74 +45,98 @@ function doTransaction(publicAddress) {
     
             }
         })
-        
+
         }).catch(console.error)
 
         }).catch(console.error);
 
     }).catch(console.error);
-}
    
-//function used to disconnect from the server and end the process
-function end() {
-    api.disconnect().then(() => {
-        console.log('API has disconnected');
-        exit()
-        process.exit();
-    })
-}
-  
-//function used to check if the transaction has been succesfully put on the ledger. 
-//It can take a couple of ledger updates for this to happen so this function will throw errors.
-//and needs to be called multiple times. 
-async function logTransaction(earliestLedgerVersion) {
-    try {
-        tx = await api.getTransaction(txID, {minLedgerVersion: earliestLedgerVersion})
-        console.log("Transaction result:", tx.outcome.result)
-        console.log("Balance changes:", JSON.stringify(tx.outcome.balanceChanges) + '\n')
-        end()  
-      } catch(error) {
-        console.log("Couldn't get transaction outcome:", error + '\n')
+    //function used to disconnect from the server and end the process
+    function end() {
+        api.disconnect().then(() => {
+            console.log('API has disconnected');
+            return false;
+        })
+    }
+    
+    //function used to check if the transaction has been succesfully put on the ledger. 
+    //It can take a couple of ledger updates for this to happen so this function will throw errors.
+    //and needs to be called multiple times. 
+    async function logTransaction(earliestLedgerVersion) {
+        try {
+            tx = await api.getTransaction(txID, {minLedgerVersion: earliestLedgerVersion})
+            console.log("Transaction result:", tx.outcome.result)
+            console.log("Balance changes:", JSON.stringify(tx.outcome.balanceChanges) + '\n')
+            end()  
+        } catch(error) {
+            console.log("Couldn't get transaction outcome:", error + '\n')
+        }
+    }
+
+    //function used to create the transaction JSON file that needs to be submitted.
+    //for a transaction to happen.
+    async function doPrepare() {
+        const sender = senderAddress
+        const preparedTx = await api.prepareTransaction({
+        "TransactionType": "Payment",
+        "Account": sender,
+        "Amount": api.xrpToDrops("0.01"), // Same as "Amount": "22000000"
+        "Destination": receiverAddress
+        }, {
+        // Expire this transaction if it doesn't execute within ~5 minutes:
+        "maxLedgerVersionOffset": 75
+        })
+        maxLedgerVersion = preparedTx.instructions.maxLedgerVersion
+        minLedgerVersion = preparedTx.instructions.minLedgerVersion
+        console.log("Prepared transaction instructions:", preparedTx.txJSON + '\n')
+        console.log("Transaction cost:", preparedTx.instructions.fee, "XRP")
+        console.log("Transaction expires after ledger:", maxLedgerVersion + '\n')
+        return preparedTx.txJSON
+    }
+
+    //the function used to submit the transaction to the ledger.
+    async function doSubmit(txBlob) {
+
+        const latestLedgerVersion = await api.getLedgerVersion()
+    
+        const result = await api.submit(txBlob)
+        
+        //log the tentative results. keep in mind that these are not final.
+        //this log could indicate a succesfull transaction that ultimately fails
+        //and vice versa.
+        console.log("Tentative result code:", result.resultCode)
+        console.log("Tentative result message:", result.resultMessage + '\n')
+    
+        // Return the earliest ledger index this transaction could appear in
+        // as a result of this submission, which is the first one after the
+        // validated ledger at time of submission.
+        return latestLedgerVersion + 1
     }
 }
 
-//function used to create the transaction JSON file that needs to be submitted.
-//for a transaction to happen.
-async function doPrepare() {
-    const sender = "rB76Ts8bXWnZVkqbFULYpwV9CaUaCFZ6hn"
-    const preparedTx = await api.prepareTransaction({
-      "TransactionType": "Payment",
-      "Account": sender,
-      "Amount": api.xrpToDrops("22"), // Same as "Amount": "22000000"
-      "Destination": "rUCzEr6jrEyMpjhs4wSdQdz4g8Y382NxfM"
-    }, {
-      // Expire this transaction if it doesn't execute within ~5 minutes:
-      "maxLedgerVersionOffset": 75
-    })
-    maxLedgerVersion = preparedTx.instructions.maxLedgerVersion
-    minLedgerVersion = preparedTx.instructions.minLedgerVersion
-    console.log("Prepared transaction instructions:", preparedTx.txJSON + '\n')
-    console.log("Transaction cost:", preparedTx.instructions.fee, "XRP")
-    console.log("Transaction expires after ledger:", maxLedgerVersion + '\n')
-    return preparedTx.txJSON
-}
-
-//the function used to submit the transaction to the ledger.
-async function doSubmit(txBlob) {
-
-    const latestLedgerVersion = await api.getLedgerVersion()
-  
-    const result = await api.submit(txBlob)
-    
-    //log the tentative results. keep in mind that these are not final.
-    //this log could indicate a succesfull transaction that ultimately fails
-    //and vice versa.
-    console.log("Tentative result code:", result.resultCode)
-    console.log("Tentative result message:", result.resultMessage + '\n')
-  
-    // Return the earliest ledger index this transaction could appear in
-    // as a result of this submission, which is the first one after the
-    // validated ledger at time of submission.
-    return latestLedgerVersion + 1
-}
+function printXrpConnection(userData) {
+    var api = new ripple.RippleAPI({server:'wss://s1.ripple.com/'});
+    api.connect().then(function() {
+        return api.getServerInfo();
+    }).then(function(server_info) {
+      document.body.innerHTML += "<p>Connected to rippled server!</p>" +
+      "      <table>" +
+      "        <tr><th>Version</th>" +
+      "          <td>" + server_info.buildVersion + "</td></tr>" +
+      "        <tr><th>Ledgers available</th>" +
+      "          <td>" + server_info.completeLedgers + "</td></tr>" +
+      "        <tr><th>hostID</th>" +
+      "          <td>" + server_info.hostID + "</td></tr>" +
+      "        <tr><th>Most Recent Validated Ledger Seq.</th>" +
+      "          <td>" + server_info.validatedLedger.ledgerVersion + "</td></tr>" +
+      "        <tr><th>Most Recent Validated Ledger Hash</th>" +
+      "          <td>" + server_info.validatedLedger.hash + "</td></tr>" +
+      "        <tr><th>Seconds since last ledger validated</th>" +
+      "          <td>" + server_info.validatedLedger.age + "</td></tr>" +
+      "<tr><th>Sender address + private key</th>" +
+      "          <td>" + userData['publicAddress'] + " " + userData['privateKey'] + "</td></tr>" +
+      "      </table>";
+      });
+  }
 
--- tipping.js
+++ tipping.js
@@ -1,36 +1,22 @@
+showCorrectDiv()
 
-function donateMoney() {  
-  // var address = document.getElementById("address").innerHTML;
-  alert("Thank you for your donation!" + "\n \nThe tip will be send to: " + getPublicAddress())
-  printXrpConnection()
-  doTransaction(getPublicAddress())
+function getUserData() {
+    var userData = {}
+    userData.publicAddress = sessionStorage.getItem('publicAddress')
+    userData.privateKey = sessionStorage.getItem('privateKey')
+    return userData;
 }
 
-function printXrpConnection() {
-  var api = new ripple.RippleAPI({server:'wss://s1.ripple.com/'});
-  api.connect().then(function() {
-      return api.getServerInfo();
-  }).then(function(server_info) {
-  document.body.innerHTML += "<p>Connected to rippled server!</p>" +
-"      <table>" +
-"        <tr><th>Version</th>" +
-"          <td>" + server_info.buildVersion + "</td></tr>" +
-"        <tr><th>Ledgers available</th>" +
-"          <td>" + server_info.completeLedgers + "</td></tr>" +
-"        <tr><th>hostID</th>" +
-"          <td>" + server_info.hostID + "</td></tr>" +
-"        <tr><th>Most Recent Validated Ledger Seq.</th>" +
-"          <td>" + server_info.validatedLedger.ledgerVersion + "</td></tr>" +
-"        <tr><th>Most Recent Validated Ledger Hash</th>" +
-"          <td>" + server_info.validatedLedger.hash + "</td></tr>" +
-"        <tr><th>Seconds since last ledger validated</th>" +
-"          <td>" + server_info.validatedLedger.age + "</td></tr>" +
-"      </table>";
-  });
+function donateMoney() {  
+  alert("Thank you for your donation!" + "\n \nThe tip will be send to: " + getPublicAddressWebpage())
+  var userData = {}
+  userData =  getUserData()
+  printXrpConnection(userData)
+  doTransaction(getPublicAddressWebpage(), userData['publicAddress'], userData['privateKey'])
 }
 
 // Funtion to get a specifc meta value
-function getPublicAddress() {
+function getPublicAddressWebpage() {
   const metas = document.getElementsByTagName('meta');
   for (let i = 0; i < metas.length; i++) {
     if (metas[i].getAttribute('name') === 'Tudelft-tipping-extension') {
@@ -40,14 +26,78 @@ function getPublicAddress() {
   return 'The public address of this webpage is not found';
 }
 
-document.getElementById("popupContainer").innerHTML = `<div class="popup" style="position: absolute;  top: 80px;
-right: 80px; width:40%;">
-<div style="position:absolute; top: 50%;left: 50%; border-style: solid; background-color: lightblue;">
+// Method to check wheter the user has uploaded their files
+function showCorrectDiv(){
+  userData = getUserData()
+  pKey = userData['privateKey']
+  pAddress = userData['publicAddress']
+  if(pAddress != null && pKey != null && pAddress != 'pAddress' && pKey != 'pKey') {
+    // Tipping could be done
+    showTipDiv()
+  } else {
+    // Upload of files need to be done first
+    showUploadDiv()
+  }
+}
+
+// Show div to upload files(public address and private key)
+function showUploadDiv() {
+  document.getElementById("popupContainer").innerHTML = `
+  <div class="popup" style="position: absolute;  top: 80px; right: 80px; width:60%;">
+  <div style="position:absolute; top: 50%;left: 50%; border-style: solid; background-color: lightblue; padding:10px">
+  <span class="popuptext" id="myPopup" style"text-align:center;"><b> Please upload two files(public address, private key) </b></span>
+  <br>
+  <br>
+  <label for="pAddress">Public address:</label>
+  <input type="file" id="publicAddress" name="publicAddress" accept=".txt">
+  <br>
+  <label for="pKey">Private address:</label>
+  <input type="file" id="privateKey" name="privateKey" accept=".txt">
+  <br>
+  <br>
+  <button type = "button" id = "uploadButton"> Upload </button>  
+  </span>
+  </div>
+  </div>`;
+
+  document.getElementById('publicAddress').addEventListener('change', readAllFiles, false);
+  document.getElementById('privateKey').addEventListener('change', readAllFiles, false);
+  document.getElementById("uploadButton").addEventListener("click", showTipDiv);
+}
+
+// Show the div to tip the webpage
+function showTipDiv() {
+  document.getElementById("popupContainer").innerHTML = `<div class="popup" style="position: absolute;  top: 80px;
+  right: 80px; width:40%;">
+  <div style="position:absolute; top: 50%;left: 50%; border-style: solid; background-color: lightblue;">
   <span class="popuptext" id="myPopup" style"text-align:center;"><b>Do you want to tip?</b></span>
   <br>
   <button type = "button" id = "donateButton"> Tip here </button>  
   </span>
   </div>
-</div>`;
+  </div>`;
+  document.getElementById('donateButton').addEventListener('click', donateMoney);
+}
 
-document.getElementById("donateButton").addEventListener("click", donateMoney);
+// Method to read the data of the txt file => in the future upload path
+function readAllFiles(evt) {
+  var files = evt.target.files, i = 0, r, f;
+  if(files[0]){
+    f=files[0]
+    r = new FileReader();
+    r.onload = (function(f){
+      return function(e){
+        console.log("Before " + sessionStorage.getItem('publicAddress'))
+        console.log("Before " + sessionStorage.getItem('privateKey'))
+        // Set the correct key, value
+        sessionStorage.setItem(String(evt.originalTarget.id), e.target.result);
+        console.log("After " + sessionStorage.getItem('publicAddress'))
+        console.log("After " + sessionStorage.getItem('privateKey'))
+        };
+      })(f);
+      r.readAsText(f);
+    } 
+    else {
+      alert("Error loading files");
+    } 
+}
