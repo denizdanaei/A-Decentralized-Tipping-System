c5b09de6326efec1a6eaa31c75f904d3e5d19b8e
denizdanaie <denizdanaie@gmail.com>
2020-03-12 14:38:22 +0100

    Content Security Policy:  (“default-src”).

--- popup/widget.js
+++ popup/widget.js
@@ -1,27 +1,20 @@
 
-
+function donateMoney() {  
+  // var address = document.getElementById("address").innerHTML;
+  alert("Thank you for your donation!" + "\n \nThe tip will be send to: " + getPublicAddress())
+  printXrpConnection()
+  // doTransaction(getPublicAddress())
+}
 function printXrpConnection() {
+  console.log(ripple);
   var api = new ripple.RippleAPI({server:'wss://s1.ripple.com/'});
   api.connect().then(function() {
-      return api.getServerInfo();
+    return api.getServerInfo();
   }).then(function(server_info) {
-  document.body.innerHTML += "<p>Connected to rippled server!</p>" +
-"      <table>" +
-"        <tr><th>Version</th>" +
-"          <td>" + server_info.buildVersion + "</td></tr>" +
-"        <tr><th>Ledgers available</th>" +
-"          <td>" + server_info.completeLedgers + "</td></tr>" +
-"        <tr><th>hostID</th>" +
-"          <td>" + server_info.hostID + "</td></tr>" +
-"        <tr><th>Most Recent Validated Ledger Seq.</th>" +
-"          <td>" + server_info.validatedLedger.ledgerVersion + "</td></tr>" +
-"        <tr><th>Most Recent Validated Ledger Hash</th>" +
-"          <td>" + server_info.validatedLedger.hash + "</td></tr>" +
-"        <tr><th>Seconds since last ledger validated</th>" +
-"          <td>" + server_info.validatedLedger.age + "</td></tr>" +
-"      </table>";
-  });
+    document.getElementById("accounInfo").innerHTMLdocument.body.innerHTML += "Connected to rippled server!"});
+
 }
+
 
 
 
