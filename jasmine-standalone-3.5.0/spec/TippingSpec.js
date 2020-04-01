ee98c6ab11fb1d7f6d23443f5d8d81d69fcfdd14
Lennart Overdevest <lennart_basketbal@msn.com>
2020-04-01 14:00:24 +0200

    Updated last version

--- jasmine-standalone-3.5.0/spec/TippingSpec.js
+++ jasmine-standalone-3.5.0/spec/TippingSpec.js
@@ -123,19 +123,9 @@ describe('Donate Money - ', function() {
 		// Spy on checking the users credentials
 		spyOn(window, 'doTransaction').and.returnValue(100);	
 		spyOn(window, 'exchangeRate').and.returnValue(120)
-		// spyOn(window, 'showTipDiv');
-	
-		// Get credentials promise
 	})
 
-	it('Donate money - Failed characters inserted', async function() {
-		var input = document.createElement("input");
-		input.type = "number";
-		input.name = "amount"
-		input.id = "amount"
-
-		document.getElementsByTagName('body')[0].appendChild(input);
-
+	it('Donate money - Failed empty donation', async function() {
 		var dummyElement = document.createElement('button');
 		document.getElementById = jasmine.createSpy('TwitterButton').and.returnValue(dummyElement);
 
@@ -148,42 +138,44 @@ describe('Donate Money - ', function() {
 
 
 //  Want to test the case where a valid number if filled in but for some reason i am not able to mock the "var e = document.getElementById("ddlViewBy");" in the else case of donateMoney
-describe('Donate Money - ', function() {
-	beforeEach(function() {
-		// Spy on checking the users credentials
-		spyOn(window, 'doTransaction').and.returnValue(100);	
-		spyOn(window, 'exchangeRate').and.returnValue(120)
-		// spyOn(window, 'showTipDiv');
+
+// describe('Donate Money - ', function() {
+// 	beforeEach(function() {
+// 		// Spy on checking the users credentials
+// 		spyOn(window, 'doTransaction').and.returnValue(100);	
+// 		spyOn(window, 'exchangeRate').and.returnValue(120)
+// 		// spyOn(window, 'showTipDiv');
 	
-		// Get credentials promise
-	})
+// 		// Get credentials promise
+// 	})
 
-	it('Donate money - correct inserted', async function() {
-		
-		var input = document.createElement("input");
-		input.type = "number";
-		input.name = "amount"
-		input.id = "amount"
+// 	it('Donate money - correct inserted', async function() 
+// 		First try
+// 		var input = document.createElement("input");
+// 		input.type = "number";
+// 		input.name = "amount"
+// 		input.id = "amount"
 
-		document.getElementsByTagName('body')[0].appendChild(input);
+// 		document.getElementsByTagName('body')[0].appendChild(input);
 
-		var dummyElement = document.createElement('button');
-		document.getElementById = jasmine.createSpy('TwitterButton').and.returnValue(dummyElement);
+// 		Second try
+// 		var dummyElement = document.createElement('button');
+// 		document.getElementById = jasmine.createSpy('TwitterButton').and.returnValue(dummyElement);
 
-		var dummyElement = document.createElement('number');
-		document.getElementById = jasmine.createSpy('amount').and.returnValue(10);
+// 		var dummyElement = document.createElement('number');
+// 		document.getElementById = jasmine.createSpy('amount').and.returnValue(10);
 
-		var dummyElement = document.createElement('select');
-		document.getElementById.value = jasmine.createSpy('ddlViewBy').and.returnValue('EUR ');
+// 		var dummyElement = document.createElement('select');
+// 		document.getElementById.value = jasmine.createSpy('ddlViewBy').and.returnValue('EUR ');
 
-		const spy = spyOnProperty(document, 'amount', 'get').andReturn(1);
+// 		const spy = spyOnProperty(document, 'amount', 'get').andReturn(1);
 
-		// spyOn(window, 'exchangeRate');
-		donateMoney()
-		expect(window.exchangeRate).toHaveBeenCalledWith(1, 'https://www.bitstamp.net/api/v2/ticker/xrpeur/', "EUR ");
+// 		// spyOn(window, 'exchangeRate');
+// 		donateMoney()
+// 		expect(window.exchangeRate).toHaveBeenCalledWith(1, 'https://www.bitstamp.net/api/v2/ticker/xrpeur/', "EUR ");
 
-	});
-});// WHY IS THIS NOT CALLED?!
+// 	});
+// });// WHY IS THIS NOT CALLED?!
 	// it('Get public empty file', async function() {
 	// 	expect(window.showTipDiv).toHaveBeenCalled()
 	// });
